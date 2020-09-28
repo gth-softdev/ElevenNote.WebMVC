36eca215790b3bb3cbcd036e9cc24287f8d8bfb1
gth-softdev <68547258+gth-softdev@users.noreply.github.com>
2020-09-28 18:55:48 -0400

    starting category2

--- ElevenNote.Data/Category.cs
+++ ElevenNote.Data/Category.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenNote.Data
 {
-    class Category
+    public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
     }
 }
